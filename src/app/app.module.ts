b3c54f9fb125d70a1e47b8b73d9f1b79f058a0cb
jsongo <jsongo@qq.com>
2017-09-20 14:30:10 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IconListComponent } from './icon-list/icon-list.component';
 import { IconComponent } from './icon/icon.component';
-import { IconDirective } from './icon/icon.directive';
+import { TooltipDirective } from './icon/icon.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     IconListComponent,
     IconComponent,
-    IconDirective
+    TooltipDirective
   ],
   imports: [
     BrowserModule
